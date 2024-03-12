6a09d406be7c1a8e8e0f7688c61ec2265b8e93de
Sebastian <andre.samame@utec.edu.pe>
2024-03-12 11:40:23 -0500

    primer commmit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="mx-auto">
+
+
+	<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+
+
+</div>
+
+<?php
+get_footer();
---
+++ template-parts/content.php
@@ -0,0 +1,127 @@
+<!-- PRIMERA SECCION -->
+
+<div id="main-content">
+    <section id="fullscreen-image" class="relative h-screen overflow-hidden">
+	<div class="absolute inset-0 bg-cover bg-center h-full w-full blur-sm z-0" style="background-image: url('<?php echo get_template_directory_uri(); ?>/resources/images/prueba.jpg');"></div>
+	<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-99 flex items-center justify-center h-full text-center">
+            <div class="w-0.6 pl-8">
+                <h1 class="text-white font-bold text-4xl mb-8">
+                    REVISTA POLICIAL
+                </h1>
+                <p class="text-white md:text-xl dark:text-gray-400 text-left text-2xl mb-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste blanditiis nemo in. Aliquid eos, id fugit odit et, blanditiis est, perferendis voluptate ab harum voluptatum facere asperiores consectetur? Aliquid, cupiditate?
+                </p>
+            </div>
+        </div>
+        <header class="text-white h-16 flex items-center justify-center w-full bg-black/[0.1] relative z-[100]">
+            <div class="w-full max-w-4xl mx-auto">
+                <nav class="flex items-center justify-center relative">
+                    <ul class="flex space-x-20 items-center h-full relative">
+                        <li>
+                            <a href="#" class="hover:text-yellow-500 hover:border-b-2 border-yellow-500 transition duration-300 ease-in-out pb-5">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-yellow-500 hover:border-b-2 border-yellow-500 transition duration-300 ease-in-out pb-5">About</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-yellow-500 hover:border-b-2 border-yellow-500 transition duration-300 ease-in-out pb-5">Products</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-yellow-500 hover:border-b-2 border-yellow-500 transition duration-300 ease-in-out pb-5">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </section>
+</div>
+</div>
+
+<!-- SEGUNDA SECCION -->
+
+<div class="contain bg-green-800 text-white py-20">
+    <div class="max-w-4xl mx-auto text-center">
+        <h2 class="text-3xl font-bold mb-8">Otro Contenido</h2>
+        <p class="text-lg mb-8">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quod eos dignissimos pariatur praesentium, distinctio in, voluptate placeat adipisci, aut itaque amet corporis aperiam eius quia reiciendis quo culpa totam.        </p>
+    </div>
+
+	<!-- TARJETAS -->
+
+	<div class="h-full mt-16 p-8">
+    <div class="container mx-auto grid grid-cols-3 gap-8">
+        <div class="relative bg-green-800 group hover:bg-white cursor-pointer transition ease-out duration-300">
+            <img src="https://i.ibb.co/FzkhpcD/pngegg.png" alt="Product 1">
+            <div class="p-4">
+                <h1 class="text-3xl font-bold group-hover:text-black transition ease-out duration-300">01.</h1>
+                <h2 class="text-1xl mt-4 font-bold group-hover:text-black">Roof light lamp</h2>
+                <p class="mt-2 opacity-60 text-white group-hover:text-black">Diverse collection of roof lights of quality</p>
+            </div>
+        </div>
+
+
+        <div class="relative bg-green-800 group hover:bg-white cursor-pointer transition ease-out duration-300">
+            <img src="https://i.ibb.co/JB4GWMJ/pngegg-1.png" alt="Product 2">
+            <div class="p-4">
+                <h1 class="text-3xl font-bold group-hover:text-black transition ease-out duration-300">02.</h1>
+                <h2 class="text-1xl mt-4 font-bold group-hover:text-black">Lounge Chair</h2>
+                <p class="mt-2 opacity-60 text-white group-hover:text-black">Comfortable collection of perfect lounge chairs</p>
+            </div>
+        </div>
+
+        <div class="relative bg-green-800 group hover:bg-white cursor-pointer transition ease-out duration-300">
+            <img src="https://i.ibb.co/MgnH44p/pngegg-2.png" alt="Product 3">
+            <div class="p-4">
+                <h1 class="text-3xl font-bold group-hover:text-black transition ease-out duration-300">03.</h1>
+                <h2 class="text-1xl mt-4 font-bold group-hover:text-black">Scandinavia Couch</h2>
+                <p class="mt-2 opacity-60 text-white group-hover:text-black">Best selection of Scandinavia couch for your home</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+<!-- TERCERA SECCION -->
+
+<div class="contain bg-white py-16">
+    <div class="max-w-4xl mx-auto text-center mb-12">
+        <h2 class="text-6xl font-bold mb-8">JANO CHICORITA</h2>
+        <!-- <p class="text-lg mb-8">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus nam id vel sequi voluptatum incidunt modi reprehenderit perferendis earum, illum deleniti molestiae placeat laborum qui eius sunt! Laudantium, placeat.
+        </p> -->
+    </div>
+
+
+
+</div>
+
+
+<!-- CUARTA SECCION -->
+<div id="history-section" class="container mx-auto text-center my-8">
+    <div class="flex flex-col md:flex-row items-center">
+        <!-- Columna Izquierda -->
+        <div class="md:w-1/2 p-4 text-center">
+            <div class="history-title mb-4">
+                <span class="combined-title text-gray-700 font-bold text-2xl">
+                    Nuestra historia
+                </span>
+            </div>
+
+            <p class="text-gray-600 leading-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis similique illum cumque perspiciatis repudiandae voluptate at doloremque. Culpa, facere voluptatem itaque earum, totam voluptate perspiciatis consequuntur blanditiis, vero deserunt velit!
+            </p>
+
+            <a href="#" class="btn mt-4 bg-gray-700 text-white py-2 px-4 rounded-full inline-block hover:bg-gray-800 transition duration-300">Leer Más</a>
+        </div>
+
+        <!-- Columna Derecha -->
+        <div class="md:w-1/2 p-4">
+            <div class="history-image">
+                <img src="<?php echo get_template_directory_uri(); ?>/resources/images/prueba2.jpg" class="w-full h-full object-cover rounded-md" alt="Historia Imagen">
+            </div>
+        </div>
+    </div>
+</div>
+
